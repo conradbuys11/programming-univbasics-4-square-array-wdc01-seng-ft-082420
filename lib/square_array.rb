c58afc2b4ef77a37c7dc2f74b60b8c4563ef801c
Conrad Buys <conradbuys11@gmail.com>
2020-08-08 14:45:17 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
+  new_array = []
   counter = 0
   while counter < array.length do
-    array[counter] ** 2
+    new_array.push(array[counter] ** 2)
     counter += 1
   end
 end
